9eda4cbfe8e4bf48b02e885b58610bd18975a0e1
joachim.segala <joachim.segala@usharesoft.com>
2017-12-11 13:19:21 +0100

    Add Gildedrose refactoring kata

---
+++ gildedrose-refactoring-kata/src/main/java/org/kata/gildedrose/Item.java
@@ -0,0 +1,16 @@
+package org.kata.gildedrose;
+
+public class Item {
+
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+}
