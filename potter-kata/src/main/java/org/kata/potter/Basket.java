8e4bcd75149960dc3fc7c6e64a6010028d491075
joachim.segala <joachim.segala@usharesoft.com>
2018-10-17 13:42:39 +0200

    Add potter-kata sources

---
+++ potter-kata/src/main/java/org/kata/potter/Basket.java
@@ -0,0 +1,16 @@
+package org.kata.potter;
+
+public class Basket {
+
+    public Basket() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addAll(Book... newBooks) {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ potter-kata/src/main/java/org/kata/potter/Book.java
@@ -0,0 +1,9 @@
+package org.kata.potter;
+
+public enum Book {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE
+}
